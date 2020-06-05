cd1ccb456992c47f5ed6e0ce05ef308627f0cbfe
Pankaj Kumar Gautam <pnkjgautam05@gmail.com>
2020-06-05 17:02:01 +0530

    adding haptic feedback on button click

--- app/src/main/java/com/example/rollit/MainActivity.kt
+++ app/src/main/java/com/example/rollit/MainActivity.kt
@@ -2,24 +2,21 @@ package com.example.rollit
 
 import android.graphics.PorterDuff
 import android.media.MediaPlayer
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Looper
+import android.os.*
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import org.w3c.dom.Text
-import java.util.Timer
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
     //Use lateinit to extract the image view variable:
-    lateinit var diceImage: ImageView
-    lateinit var mediaPlayer: MediaPlayer
+    private lateinit var diceImage: ImageView
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var vibrator: Vibrator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,23 +24,30 @@ class MainActivity : AppCompatActivity() {
         //taking id's for button and image
         val rollButton: Button = findViewById(R.id.button)
         diceImage = findViewById(R.id.dice_image)
+        vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
         //step-1 adding messages
-        var diceNumMessage = listOf(
+        val diceNumMessage = listOf(
             "Oops! You got 1. Better luck next time." + getEmojiByUnicode(0x1F61E),
             "You got 2.",
             "You got 3.",
             "You got 4. Try again for 6.",
             "You got 5. So close to get a 6!",
             "Hurray! You got 6." + getEmojiByUnicode(0x1F389)
-        );
+        )
         rollButton.setOnClickListener {
             //rollButton.setEnabled(false)
             val dice = Dice(6)
             val diceRoll = dice.roll()
+            //adding haptic feedback
+            if(Build.VERSION.SDK_INT>=26) {
+                vibrator.vibrate(VibrationEffect.createOneShot(75, 128))
+            }
             //adding a toast on button click
-            var toast:Toast = Toast.makeText(this, "Rolling!", Toast.LENGTH_SHORT);
-            val makeToast = MakeToast(toast)
-            makeToast.toast()
+
+            val toastRoll:Toast = Toast.makeText(this, "Rolling!", Toast.LENGTH_SHORT)
+            toastRoll.show()
+            //val makeToast = MakeToast(toast)
+            //makeToast.toast()
             //step-2 selecting media file
             val mediaResource = if (diceRoll > 3) R.raw.dice_roll_win else R.raw.dice_roll_lose
             //step-3 adding background sound
@@ -59,9 +63,10 @@ class MainActivity : AppCompatActivity() {
                 rollDice(diceRoll)
                 Looper.prepare()
                 //toast
-                var toast:Toast = Toast.makeText(this@MainActivity, diceNumMessage[diceRoll - 1], Toast.LENGTH_LONG)
-                val makeToast = MakeToast(toast)
-                makeToast.toast()
+                val toastDice:Toast = Toast.makeText(this@MainActivity, diceNumMessage[diceRoll - 1], Toast.LENGTH_LONG)
+                toastDice.show()
+                //val makeToast = MakeToast(toast)
+                //makeToast.toast()
                 Looper.loop()
             }
             //set-enabled the button
@@ -88,7 +93,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getEmojiByUnicode(unicode: Int): String {
-        return String(Character.toChars(unicode));
+        return String(Character.toChars(unicode))
     }
 }
 
